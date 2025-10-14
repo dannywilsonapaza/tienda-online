dfb1a796a9826bf924e58a0ee08c3b5f92e19708
Danny <dannyapaza40@gmail.com>
2025-10-14 12:48:49 -0500

    guardianes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { ListadoProductosComponent } from './listado-productos/listado-productos
 import { FormularioProductoComponent } from './formulario-producto/formulario-producto.component';
 import { ErrorComponent } from './error/error.component';
 import { LoginComponent } from './login/login.component';
+import { LoginGuardianService } from './login-guardian.service';
 
 export const routes: Routes = [
-  {path:'', component: ListadoProductosComponent},
-  {path:'listado', component: ListadoProductosComponent},
-  {path:'agregar', component: FormularioProductoComponent},
-  {path:'editar/:llave', component: FormularioProductoComponent},
+  {path:'', component: ListadoProductosComponent, canActivate: [LoginGuardianService]},
+  {path:'listado', component: ListadoProductosComponent, canActivate: [LoginGuardianService]},
+  {path:'agregar', component: FormularioProductoComponent, canActivate: [LoginGuardianService]},
+  {path:'editar/:llave', component: FormularioProductoComponent, canActivate: [LoginGuardianService]},
   {path:'login', component: LoginComponent},
   //Ruta comodin para cualquier otra ruta no definida
   {path:'**', component: ErrorComponent}
---
+++ src/app/login-guardian.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { LoginService } from './login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuardianService implements CanActivate {
+
+  constructor(private loginService: LoginService,
+    private router: Router
+
+  ) { }
+
+  //Verificar si el usuario está autenticado antes de activar la ruta
+  canActivate(): boolean {
+    if (this.loginService.isAutenticado()){
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
