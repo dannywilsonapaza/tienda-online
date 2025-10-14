9f57eb89602abea0a99f6d121f190dde8e678d52
Danny <dannyapaza40@gmail.com>
2025-10-14 11:14:06 -0500

    Login creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { ListadoProductosComponent } from './listado-productos/listado-productos.component';
 import { FormularioProductoComponent } from './formulario-producto/formulario-producto.component';
 import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {path:'', component: ListadoProductosComponent},
   {path:'listado', component: ListadoProductosComponent},
   {path:'agregar', component: FormularioProductoComponent},
   {path:'editar/:llave', component: FormularioProductoComponent},
+  {path:'login', component: LoginComponent},
   //Ruta comodin para cualquier otra ruta no definida
   {path:'**', component: ErrorComponent}
 
